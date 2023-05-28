12c81f2d433c9077c401194371e2861446dc0ef7
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-28 22:06:42 +0500

    //

---
+++ JS-Concepts.js
@@ -0,0 +1,24 @@
+---------------------------------------------------------------------------------------------------------------------------------------------------
+                                                    Double Equal == VS Triple Equal ===
+== checks for the value only
+=== checks for the value and also its types
+
+
+let x = 5
+let y = 5
+let z = '5'
+
+
+document.write(x==z) //true
+document.write(x===z) //false
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+                                                      FIND METHOD() VS FILTER() METHOD
+
+find() method# return the first value which match from the collection.
+It search the array from the start if the desired value is matched then find() method return that value and terminate and rest of the array is not process.
+
+filter() method returns the match value from the array. 
+It search whole array from start to end and returns all the value which is matched.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
--- Single-Double-Equal-Difference.js
+++
@@ -1,9 +0,0 @@
-// == checks for the value only
-// === checks for the value and also its types
-let x = 5
-let y = 5
-let z = '5'
-
-
-document.write(x==z) //true
-document.write(x===z) //false
